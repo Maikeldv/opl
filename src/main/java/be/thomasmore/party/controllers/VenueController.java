24b98ca5b2f80c606cb5ebe50d22d2323bbfc188
Maikeldv <71980625+Maikeldv@users.noreply.github.com>
2022-02-22 16:15:44 +0100

    aanpassing 3

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -15,9 +15,9 @@ public class VenueController {
     @Autowired
     private VenueRepository venueRepository;
 
-    @GetMapping("/venuedetails/{id}")
-    public String venuedetails(Model model, @PathVariable int id) {
-
+    @GetMapping({"/venuedetails/{id}", "/venuedetails"})
+    public String venuedetails(Model model, @PathVariable(required = false) Integer id) {
+    if (id == null) return "venuedetails";
         //Venue venue = new Venue("Boesj", "https://www.facebook.com/boesjkammeree/", 100, false, true, false, false, "Mechelen", 1);
         Optional<Venue> venueFromDb = venueRepository.findById(id);
         //noinspection OptionalIsPresent
