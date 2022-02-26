03fb3c37fbc112ec97366f65180d3a37bfb1e3ee
Maikel <maikel.devos@hotmail.com>
2022-02-26 10:17:31 +0100

    outdoor functie

--- src/main/java/be/thomasmore/party/controllers/VenueController.java
+++ src/main/java/be/thomasmore/party/controllers/VenueController.java
@@ -34,5 +34,11 @@ public class VenueController {
 
         return "venuelist";
     }
+    @GetMapping("/venuelist/outdoor/yes")
+    public String venuelistOutdoorYes(Model model){
+        Iterable<Venue> venues = venueRepository.findByOutdoor(true);
+        model.addAttribute("venues",venues);
+        return"venuelist";
+    }
 
 }
